fe58459faf3ad271c288f5fcfc37d4cd66947388
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-04-19 20:29:35 -0500

    Able to apply masks.

---
+++ JaysModFramework/Clothing/Components/MaleMasks.cs
@@ -0,0 +1,8 @@
+﻿namespace JaysModFramework.Clothing.Components
+{
+    public static class MaleMasks
+    {
+        public readonly static Mask Combat = new Mask("Combat", 35, new OutfitColor[] { new OutfitColor("Black", 0) });
+        public readonly static Mask Scuba = new Mask("Scuba Hood", 122, new OutfitColor[]{ new OutfitColor("Black", 0) });
+    }
+}
