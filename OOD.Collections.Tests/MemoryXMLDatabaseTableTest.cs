c0d012868335dfae7ba096e8030f0f1c54267bc7
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-04-25 10:40:12 -0500

    Extract XMLDatabaseTable

---
+++ OOD.Collections.Tests/MemoryXMLDatabaseTableTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace OOD.Collections.Tests
+{
+    using Table = MemoryXMLDatabaseTable<string, TestItem>;
+    public class TestItem : IXMLDatabaseItem<string>
+    {
+        public string ID { get; set; } = "ID string";
+        public string Value { get; set; } = "Value string";
+        public TestItem() { }
+        public TestItem(string id, string value)
+        {
+            ID = id;
+            Value = value;
+        }
+    }
+    [TestClass]
+    public class MemoryXMLDatabaseTableTest
+    {
+        private static string DIRECTORY { get; } = Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\TestValues");
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Table table = new Table(DIRECTORY, "read");
+        }
+        [TestMethod]
+        public void TestAdd()
+        {
+            Table table = new Table(DIRECTORY, "read");
+            table.AddValue(new TestItem());
+            Assert.IsTrue(table.TryGetValue(new TestItem().ID, out TestItem value));
+        }
+        [TestMethod]
+        public void TestClearCache()
+        {
+            Table table = new Table(DIRECTORY, "read");
+            table.AddValue(new TestItem());
+            table.ClearCache();
+            Assert.IsTrue(table.TryGetValue(new TestItem().ID, out TestItem value));
+        }
+    }
+}
---
+++ OOD.Collections/MemoryXMLDatabaseTable.cs
@@ -0,0 +1,107 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+
+namespace OOD.Collections
+{
+    public class MemoryXMLDatabaseTable<TKey, TValue>: XMLDatabaseTable<TKey, TValue> where TValue : IXMLDatabaseItem<TKey>
+    {
+        #region Properties
+        private Dictionary<TKey, TValue> _values = new Dictionary<TKey, TValue>();
+        public override int Count { get { return _values.Count; } }
+        private string _filepath { get { return Path.Combine(Directory, TableName + ".xml"); } }
+        #endregion
+        #region Constructors
+        public MemoryXMLDatabaseTable(string directory, string tableName)
+        {
+            Directory = directory;
+            TableName = tableName;
+            Load();
+        }
+        #endregion Constructors
+        #region GetValue
+        public override bool TryGetValue(TKey ID, out TValue value)
+        {
+            return _values.TryGetValue(ID, out value);
+        }
+        public override TValue GetValue(TKey ID)
+        {
+            if (TryGetValue(ID, out TValue value))
+            {
+                return value;
+            }
+            return value;
+        }
+        public override bool Contains(TKey ID)
+        {
+            return TryGetValue(ID, out TValue value);
+        }
+        #endregion Get
+        #region AddValue
+        public override bool TryAddValue(TValue value)
+        {
+            if (_values.TryAdd(value.ID, value))
+            {
+                Save();
+                return true;
+            }
+            return false;
+        }
+        public override bool TryUpdateValue(TValue value)
+        {
+            if (Contains(value.ID)) return false;
+            RemoveValue(value.ID);
+            AddValue(value);
+            return true;
+        }
+        #endregion AddValue
+        #region RemoveValue
+        public override bool TryRemoveValue(TKey id)
+        {
+            return _values.TryRemove(id);
+        }
+        #endregion RemoveValue
+        public override void ClearCache()
+        {
+            Load();
+        }
+        private void Load()
+        {
+            _values.Clear();
+            if (!File.Exists(_filepath)) return;
+            _values = new Dictionary<TKey, TValue>();
+            using (TextReader reader = new StreamReader(_filepath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(TValue[]));
+                TValue[] valueArray;
+                try
+                {
+                    valueArray = (TValue[])serializer.Deserialize(reader);
+                }
+                catch
+                {
+                    valueArray = null;
+                }
+                if (valueArray == null) return;
+                foreach (TValue value in valueArray)
+                {
+                    if (value != null)
+                    {
+                        _values.TryAdd(value.ID, value);
+                    }
+                }
+            }
+        }
+        private void Save()
+        {
+            System.IO.Directory.CreateDirectory(Directory);
+            using (FileStream stream = File.Create(_filepath)) { }
+
+            using (TextWriter writer = new StreamWriter(_filepath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(TValue[]));
+                serializer.Serialize(writer, _values.ValueArray);
+            }
+        }
+    }
+}
