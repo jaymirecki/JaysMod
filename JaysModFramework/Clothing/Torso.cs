8a5dd47b00abb6af17803b6329d20b722170167b
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-04-24 23:56:28 -0500

    Update Torso.cs

--- JaysModFramework/Clothing/Torso.cs
+++ JaysModFramework/Clothing/Torso.cs
@@ -10,10 +10,49 @@ namespace JaysModFramework.Clothing
         #region Properties
         public string ID { get; }
         public string Name { get; }
+        public Gender Sex { get; }
+        #region Components
         private OutfitComponent _arms;
+        private string _armsString
+        {
+            get { return _arms.ToJSON(); }
+            set { _arms = new OutfitComponent(value); }
+        }
+        private OutfitComponent _undershirt;
+        private string _undershirtString
+        {
+            get { return _undershirt.ToJSON(); }
+            set { _undershirt = new OutfitComponent(value); }
+        }
+        private OutfitComponent _jacket;
+        private string _jacketString
+        {
+            get { return _jacket.ToJSON(); }
+            set { _jacket = new OutfitComponent(value); }
+        }
+        private OutfitComponent _badge;
+        private string _badgeString
+        {
+            get { return _badge.ToJSON(); }
+            set { _badge = new OutfitComponent(value); }
+        }
+        private int _parachuteIndex;
+        private int _vest;
+        #endregion Components
         #endregion Properties
         #region Constructors
-        //public Torso() { }
+        public Torso(string id, string name, Gender sex, OutfitComponent arms, OutfitComponent undershirt, OutfitComponent jacket, OutfitComponent badge, int parachuteIndex, int vest)
+        {
+            ID = id;
+            Name = name;
+            Sex = sex;
+            _arms = arms;
+            _undershirt = undershirt;
+            _jacket = jacket;
+            _badge = badge;
+            _parachuteIndex = parachuteIndex;
+            _vest = vest;
+        }
         #endregion Constructors
         public void SetToNPC(NPC npc)
         {
