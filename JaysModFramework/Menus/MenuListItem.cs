b090e235b224c0c0f6e55135aea659839c6ba0f9
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-05-27 21:13:15 -0500

    Create modules and module manager for interaction menu

---
+++ JaysModFramework/Menus/MenuListItem.cs
@@ -0,0 +1,53 @@
+﻿using LemonUI.Menus;
+using System;
+
+namespace JaysModFramework.Menus
+{
+    public class MenuListItem<T>: IMenuItem
+    {
+        internal NativeListItem<T> _nativeItem;
+        public string Title
+        {
+            get { return _nativeItem.Title; }
+            set { _nativeItem.Title = value; }
+        }
+        public MenuListItem(string title, params T[] objs)
+        {
+            _nativeItem = new NativeListItem<T>(title, objs);
+        }
+        public MenuListItem(string title, string subtitle, params T[] objs)
+        {
+            _nativeItem = new NativeListItem<T>(title, subtitle, objs);
+        }
+        public event SelectedEventHandler Selected
+        {
+            add { _nativeItem.Selected += value; }
+            remove { _nativeItem.Selected -= value; }
+        }
+        public event EventHandler Activated
+        {
+            add { _nativeItem.Activated += value; }
+            remove { _nativeItem.Activated -= value; }
+        }
+        public bool Enabled
+        {
+            get { return _nativeItem.Enabled; }
+            set { _nativeItem.Enabled = value; }
+        }
+        public event ItemChangedEventHandler<T> ItemChanged
+        {
+            add { _nativeItem.ItemChanged += value; }
+            remove { _nativeItem.ItemChanged -= value; }
+        }
+        public T SelectedItem
+        {
+            get { return _nativeItem.SelectedItem; }
+            set { _nativeItem.SelectedItem = value; }
+        }
+        public int SelectedIndex
+        {
+            get { return _nativeItem.SelectedIndex; }
+            set { _nativeItem.SelectedIndex = value; }
+        }
+    }
+}
---
+++ JaysModFramework/ModuleManager.cs
@@ -0,0 +1,51 @@
+﻿using JaysModFramework.Menus;
+using LemonUI;
+using System.Collections.Generic;
+
+namespace JaysModFramework
+{
+    using ModuleListItem = MenuListItem<string>;
+    public static class ModuleManager
+    {
+        private static readonly List<Module> _moduleList = new List<Module>();
+        private static readonly List<ModuleListItem> _moduleMenuItemList = new List<ModuleListItem>();
+        private static Menu _moduleMenu;
+        public static Menu ModuleMenu(ObjectPool pool)
+        {
+            if (_moduleMenu == null)
+            {
+                _moduleMenu = new Menu("Modules", "Modules", pool);
+                _moduleMenu.Opening += ModuleMenu_Opening;
+            }
+            return _moduleMenu;
+        }
+
+        private static void ModuleMenu_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            RefreshMenu();
+        }
+
+        private static int MenuItemComparer(ModuleListItem a, ModuleListItem b)
+        {
+            return string.Compare(a.Title, b.Title);
+        }
+        public static void AddModule(Module module)
+        {
+            _moduleList.Add(module);
+            _moduleMenuItemList.Add(module.MenuItem);
+        }
+        private static void RefreshMenu()
+        {
+            if (_moduleMenu == null)
+            {
+                return;
+            }
+            _moduleMenu.Clear();
+            _moduleMenuItemList.Sort(MenuItemComparer);
+            foreach (ModuleListItem item in _moduleMenuItemList)
+            {
+                _moduleMenu.Add(item);
+            }
+        }
+    }
+}
