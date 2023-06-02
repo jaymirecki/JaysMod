9f94de7d2eef74d451831bf49774ae53205dddf8
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-06-02 17:14:32 -0500

    Update MatterOfTime.cs

--- JaysModFramework/Modules/MatterOfTime.cs
+++ JaysModFramework/Modules/MatterOfTime.cs
@@ -5,13 +5,30 @@ namespace JaysModFramework
 {
     public class MatterOfTime: Module
     {
-        private static int Minutes = 0;
-        private static bool Activated = false;
+        private  int _minutes = 0;
 
         public override string ModuleName => "A Matter of Time";
 
         public override string ModuleDescription => "Functionality for modifying the flow of time. The Time Stone!";
 
         public override bool DefaultActivationState => false;
+        public override void OnActivate()
+        {
+            World.IsClockPaused = true;
+            _minutes = DateTime.Now.Minute;
+        }
+        public override void OnDeactivate()
+        {
+            World.IsClockPaused = false;
+        }
+        public override void OnTick()
+        {
+            if (DateTime.Now.Minute != _minutes)
+            {
+                Debug.Log(DebugSeverity.Info, _minutes.ToString());
+                World.CurrentDate = World.CurrentDate.AddMinutes(1);
+                _minutes = DateTime.Now.Minute;
+            }
+        }
     }
 }
