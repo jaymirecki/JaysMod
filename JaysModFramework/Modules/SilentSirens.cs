2c26a57b5e5a3f318791052027276ed838675264
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-06-02 16:57:54 -0500

    Rename module files

--- JaysModFramework/Modules/SilentSirens.cs
+++ JaysModFramework/Modules/SilentSirens.cs
@@ -4,7 +4,7 @@ using System.Windows.Forms;
 
 namespace JaysModFramework.Modules
 {
-    public class SirenModule : Module
+    public class SilentSirens : Module
     {
 
         public override string ModuleName => "Silent Sirens";
