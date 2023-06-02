61e6a2b1bc31e4534abff009984d8f790f6e8a9e
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-06-02 17:01:28 -0500

    Convert MatterOfTime to module

--- JaysModFramework/Modules/MatterOfTime.cs
+++ JaysModFramework/Modules/MatterOfTime.cs
@@ -1,49 +1,17 @@
 ﻿using GTA;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JaysModFramework
 {
-    public class MatterOfTime: Script
+    public class MatterOfTime: Module
     {
         private static int Minutes = 0;
         private static bool Activated = false;
 
-        public MatterOfTime()
-        {
-            Tick += OnTick;
-        }
+        public override string ModuleName => "A Matter of Time";
 
-        private void OnTick(object sender, EventArgs e)
-        {
-            if (Activated)
-            {
-                if (DateTime.Now.Minute != Minutes)
-                {
-                    World.CurrentDate = World.CurrentDate.AddMinutes(1);
-                    Minutes = DateTime.Now.Minute;
-                }
-            }
-        }
-        public static void Activate()
-        {
-            Activated = true;
-            Minutes = DateTime.Now.Minute;
-        }
-        public static void Deactivate()
-        {
-            Activated = false;
-        }
-        public static void Toggle()
-        {
-            Activated = !Activated;
-            if (Activated)
-            {
-                Minutes = DateTime.Now.Minute;
-            }
-        }
+        public override string ModuleDescription => "Functionality for modifying the flow of time. The Time Stone!";
+
+        public override bool DefaultActivationState => false;
     }
 }
