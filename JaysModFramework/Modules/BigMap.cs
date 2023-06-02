fb77fc087257bfbea7c3a89e05ff2e71ec1d4109
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-06-02 17:30:03 -0500

    Create SemanticVersion struct

--- JaysModFramework/Modules/BigMap.cs
+++ JaysModFramework/Modules/BigMap.cs
@@ -7,9 +7,7 @@ namespace JaysModFramework.Modules
 {
     public class BigMapModule : Module
     {
-        internal override int MajorVersion => 1;
-        internal override int MinorVersion => 0;
-        internal override int PatchVersion => 0;
+        internal override SemanticVersion Version { get; } = new SemanticVersion(1, 0, 0);
         public override string ModuleName { get; } = "Big Map SP";
         public override string ModuleDescription { get; } = "Enables use of larger radar from MP";
         public override bool DefaultActivationState { get { return Global.Config.BigMapModuleEnabled; } }
--- JaysModFramework/Modules/SilentSirens.cs
+++ JaysModFramework/Modules/SilentSirens.cs
@@ -6,9 +6,7 @@ namespace JaysModFramework.Modules
 {
     public class SilentSirens : Module
     {
-        internal override int MajorVersion => 1;
-        internal override int MinorVersion => 0;
-        internal override int PatchVersion => 0;
+        internal override SemanticVersion Version { get; } = new SemanticVersion(1, 0, 0);
         public override string ModuleName => "Silent Sirens";
         public override string ModuleDescription => "Allows turning off a siren but leaving emergency lights on";
         public override bool DefaultActivationState => Global.Config.SirenModuleEnabled;
