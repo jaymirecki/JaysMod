998eef15b3d1d1be87074fee0babeb56f1a5f0dd
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2024-05-30 08:35:40 -0500

    feat: position offset based on heading and distance

--- JaysModFramework/JMF/Vector3.cs
+++ JaysModFramework/JMF/Vector3.cs
@@ -38,6 +38,44 @@ namespace JMF
         {
             return Sqrt(Pow(X - destination.X, 2) + Pow(Y - destination.Y, 2));
         }
+        public float HeadingTo(Vector3 destination)
+        {
+            return 0;
+        }
+        public Vector3 Offset(float distance, float heading)
+        {
+            float theta = GetTheta(heading);
+            float x = (float)(distance * System.Math.Cos(theta)) * (heading < 180 ? 1 : -1);
+            float y = (float)(distance * System.Math.Sin(theta)) * ((heading < 270) && (heading > 90) ? -1 : 1);
+            return new Vector3(X + x, Y + y, Z);
+        }
+        private float GetTheta(float heading)
+        {
+            float theta;
+            // Calculate angle between heading and X-axis
+            if (heading <= 90)
+            {
+                theta = 90 - heading;
+            }
+            else if (heading <= 180)
+            {
+                theta = heading - 90;
+            }
+            else if (heading < 270)
+            {
+                theta = 270 - heading;
+            }
+            else
+            {
+                theta = heading - 270;
+            }
+            // Convert to radians and return
+            return (float)(theta * (System.Math.PI / 180));
+        }
+        public Vector3 Offset(Vector3 offset)
+        {
+            return new Vector3(X + offset.X, Y + offset.Y, Z + offset.Z);
+        }
         #endregion Methods
         #region Math Helpers
         public static float Sqrt(float a)
--- JaysModFramework/JMF/World.cs
+++ JaysModFramework/JMF/World.cs
@@ -13,5 +13,9 @@ namespace JMF
         {
             set { Function.Call(Hash.SetWeatherTypeNowPersist, value.ToString().ToUpper()); }
         }
+        public static void DrawMarker(Vector3 position)
+        {
+            Function.Call(Hash.DrawMarker, 1, position.X, position.Y, position.Z, 0f, 0f, 0f, 0f, 0f, 0f, 1f, 1f, 1f, 255, 128, 0, 100, false, false, false, 2, 0, 0, false);
+        }
     }
 }
